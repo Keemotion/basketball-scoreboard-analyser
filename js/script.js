7d1f1ac0c0ad37721c689a773509a23386ae56f9
Floris Kint <floris.kint@gmail.com>
2013-10-08 20:03:41 +0200

    Initial js/script.js file

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * objects = array of {name, digit_amount, digits = array of 4 coordinates {x,y} }
+ *
+ */
+var objects = new Array();
+
+function loadObjects(){
+    objects = new Array();    
+    addObject("team1_naam1", 3);
+}
+function addObject(label_name, digit_amount){
+    var o = new Object();
+    o.name = label_name;
+    o.digit_amount = digit_amount;
+    o.digits = new Array(digit_amount);
+    for(var i = 0; i < digit_amount; ++i){
+        o.digits[i].coordinates = new Array(4);
+        for(var j = 0; j < 4; ++j){
+            o.digits[i].coordinates[j] = {null, null};
+        }
+    }
+    objects.push(o);
+}
+
+
+loadObjects();
