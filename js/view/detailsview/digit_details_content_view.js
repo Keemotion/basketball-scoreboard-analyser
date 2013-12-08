8a7f75e69bca5faff037fee6c02f7cc0f9696a30
Floris Kint <floris.kint@gmail.com>
2013-12-08 21:14:31 +0100

    Fixed digit click on canvas

--- js/view/detailsview/digit_details_content_view.js
+++ js/view/detailsview/digit_details_content_view.js
@@ -1,4 +1,25 @@
 define(["./corner_details_content_view", "../../messaging_system/event_listener"],function(CornerDetailsContentView, EventListener){
+	var CanvasClickListener = function(parentView){
+		this.parentView = parentView;
+		this.listening = false;
+	};
+	CanvasClickListener.prototype.start = function(){
+		this.listening = true;
+		this.index = 0;
+	};
+	CanvasClickListener.prototype.clickReceived = function(signal, data){
+		if(this.listening == true){
+			console.log("setting: "+data.imageX+", "+data.imageY);
+			console.log("index = "+this.index);
+			this.parentView.content_elements[this.index].setCoordinate(data.imageX, data.imageY);
+			++this.index;
+			if(this.index== 4){
+				console.log("digit was 4");
+				this.listening = false;
+				this.parentView.click_button.removeClass('active');
+			}
+		}
+	};
 	var DigitDetailsContentView = function(target_view, data_proxy, messaging_system){
 		var self = this;
 		this.messaging_system = messaging_system;
@@ -9,6 +30,8 @@ define(["./corner_details_content_view", "../../messaging_system/event_listener"
 			.append($('<span>').text('test'));
 		this.title_span = $('<span>')
 			.text('');
+		this.canvasClickListener = new CanvasClickListener(this);
+		this.messaging_system.addEventListener(this.messaging_system.events.CanvasImageClick, new EventListener(self.canvasClickListener,self.canvasClickListener.clickReceived));
 		this.click_button = $('<button>')
 			.text('click to set digit')
 			.attr({
@@ -17,22 +40,11 @@ define(["./corner_details_content_view", "../../messaging_system/event_listener"
 			.click(function(e){
 				e.preventDefault();
 				self.click_button.addClass('active');
-				self.canvasClickedEventListener = new EventListener(self, self.canvasClickedToSetCoordinate);
-				self.digitClickListenIndex = 0;
-				messaging_system.addEventListener(messaging_system.events.CanvasImageClick, self.canvasClickedEventListener);
+				self.canvasClickListener.start();
 			});
 		this.target_view
 			.append(this.content_element);
 		this.loadContent();
-	};
-	DigitDetailsContentView.prototype.canvasClickedToSetCoordinate = function(signal, data){
-		this.content_elements[this.digitClickListenIndex].setCoordinate(data.imageX, data.imageY);	
-		++this.digitClickListenIndex;
-		if(this.digitClickListenIndex == 4){
-			this.messaging_system.removeEventListener(this.messaging_system.events.CanvasImageClick, this.canvasClickedEventListener);
-			this.digitClickListenIndex = 0;
-			this.click_button.removeClass('active');
-		}
 	};
 	DigitDetailsContentView.prototype.loadContent = function(){
 		var subnodes = this.data_proxy.getSubNodes();
