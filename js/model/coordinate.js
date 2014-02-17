c6f7833f4afefc84354a2cad13e8536cb0d8dce2
Floris Kint <floris.kint@gmail.com>
2014-02-17 11:11:29 +0100

    7+2 sampling points

--- js/model/coordinate.js
+++ js/model/coordinate.js
@@ -13,5 +13,23 @@ define([],function(){
 		return this.x && this.y;
 	};
     Coordinate.prototype.type = "coordinate";
+	Coordinate.getMiddle = function(arr){
+		if(arr.length == 0)
+			return new Coordinate('', '');
+		var total_x = 0;
+		var total_y = 0;
+		for(var i = 0; i < arr.length; ++i){
+			total_x += arr[i].getX();
+			total_y += arr[i].getY();
+		}
+		total_x /= arr.length;
+		total_y /= arr.length;
+		return new Coordinate(total_x, total_y);
+	};
+	Coordinate.getSquareDistance = function(c1, c2){
+		var dx = c1.getX()-c2.getX();
+		var dy = c1.getY()-c2.getY();
+		return dx*dx+dy*dy;
+	};
     return Coordinate;
 });
--- js/view/canvas/digit_display.js
+++ js/view/canvas/digit_display.js
@@ -23,7 +23,7 @@ define(['./base_display', './corner_display', '../../model/coordinate'], functio
 		}
 		var left_middle = new Coordinate((coordinates[0].getX()+coordinates[3].getX())/2.0, (coordinates[0].getY()+coordinates[3].getY())/2.0);
 		var right_middle = new Coordinate((coordinates[1].getX()+coordinates[2].getX())/2.0, (coordinates[1].getY()+coordinates[2].getY())/2.0);
-		context.lineWidth = 5;
+		context.lineWidth = 2;
 		context.beginPath();
 		context.moveTo(left_middle.getX(), left_middle.getY());
 		context.lineTo(coordinates[0].getX(), coordinates[0].getY());
@@ -35,6 +35,39 @@ define(['./base_display', './corner_display', '../../model/coordinate'], functio
 		context.lineTo(right_middle.getX(), right_middle.getY());
 		context.strokeStyle = "#00ff00";
 		context.stroke();
+		var sampling_points = new Array();
+		sampling_points.push(Coordinate.getMiddle([coordinates[0], coordinates[1]]));
+		sampling_points.push(Coordinate.getMiddle([coordinates[1], right_middle]));
+		sampling_points.push(Coordinate.getMiddle([right_middle, coordinates[2]]));
+		sampling_points.push(Coordinate.getMiddle([coordinates[2], coordinates[3]]));
+		sampling_points.push(Coordinate.getMiddle([coordinates[3], left_middle]));
+		sampling_points.push(Coordinate.getMiddle([left_middle, coordinates[0]]));
+		sampling_points.push(Coordinate.getMiddle([right_middle, left_middle]));
+		
+		sampling_points.push(Coordinate.getMiddle([coordinates[0], coordinates[1], left_middle, right_middle]));
+		sampling_points.push(Coordinate.getMiddle([coordinates[2], coordinates[3], left_middle, right_middle]));
+		
+		//only draw if no circles intersect
+		var sampling_point_radius = 5;
+		var intersecting = false;
+		for(var i = 0; i < sampling_points.length && !intersecting; ++i){
+			for(var j = 0; j < i; ++j){
+				if(Coordinate.getSquareDistance(sampling_points[i], sampling_points[j]) <= 2*sampling_point_radius){
+					intersecting = true;
+					break;
+				}
+			}
+		}
+		if(!intersecting){
+			for(var i = 0; i < sampling_points.length; ++i){
+				context.beginPath();
+				context.arc(sampling_points[i].getX(), sampling_points[i].getY(), sampling_point_radius, 0, 2*Math.PI);
+				context.lineWidth = 3;
+				context.strokeStyle = "#00ff00";
+				context.stroke();
+			}
+		}
+		//sampling points:
 		//console.log("TODO: digitdisplay has overridden this drawMyself method, but still needs some proper implementation");
 	};
 	DigitDisplay.prototype.loadSubComponents = function(){
