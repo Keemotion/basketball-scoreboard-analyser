4b7520410afd6484e2626052bf6deafdb80602b4
Floris Kint <floris.kint@gmail.com>
2014-07-19 21:36:34 +0200

    Collapsed global configuration overview

--- js/view/detailsview/configuration_details_overview.js
+++ js/view/detailsview/configuration_details_overview.js
@@ -1,10 +1,21 @@
 define([], function(){
 	var ConfigurationDetailsOverview = function(data_proxy, target_element){
+		var self = this;
 		this.data_proxy = data_proxy;
 		this.target_element = target_element;
 		this.target_element.empty();
 		this.list_element = $('<ul>');
-		this.target_element.append($('<span>').text('Global configuration overview'));
+		this.expand_element = $('<span>&gt;</span>').addClass('expand-command').click(function(){
+			self.expand_details();
+		});
+		this.collapse_element = $('<span>v</span>').addClass('collapse-command').click(function(){
+			self.collapse_details();
+		});
+		this.title_element = $('<span>')
+			.append(this.expand_element)
+			.append(this.collapse_element)
+			.append($('<span>').html('&nbsp;Global configuration overview'));
+		this.target_element.append(this.title_element);
 		this.target_element.append(this.list_element);
 		this.loadContent();
 	};
@@ -16,9 +27,20 @@ define([], function(){
 				this.list_element.append($('<li>').text(k+" = "+configuration[k]));
 			}
 		}
+		this.collapse_details();
 	};
 	ConfigurationDetailsOverview.prototype.cleanUp = function(){
 
+	};
+	ConfigurationDetailsOverview.prototype.collapse_details = function(){
+		this.list_element.hide();
+		this.collapse_element.hide();
+		this.expand_element.show();
+	};
+	ConfigurationDetailsOverview.prototype.expand_details = function(){
+		this.list_element.show();
+		this.collapse_element.show();
+		this.expand_element.hide();
 	};
 	return ConfigurationDetailsOverview;
 });
