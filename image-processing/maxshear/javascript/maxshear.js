75503180687a781caaf4fe45433809452f3a7e4b
Floris Kint <floris.kint@gmail.com>
2014-07-27 14:26:00 +0200

    Horizontal + vertical lines in rectangular image

--- image-processing/maxshear/javascript/maxshear.js
+++ image-processing/maxshear/javascript/maxshear.js
@@ -1,18 +1,20 @@
-var img = new Image();
+var Canvas = require('canvas');
+var img = new Canvas.Image();
 img.onload = function() {
 	start();
 };
 //img.src = "../chalon-2.png";
-//img.src = "../digit2g.png";
+img.src = "../digit2g.png";
 //img.src = "../digit4-blue-pink-sm.png";
 //img.src = "../digit4-blue-pink.png";
 //img.src = "../leds-28.png";
-img.src = "../leds-34.png";
+//img.src = "../leds-34.png";
 //img.src = "../pink-blue-10.png";
 
 function start() {
-	var canvas = $('<canvas>');
-	var context = canvas[0].getContext("2d");
+	//var canvas = $('<canvas>');
+	var canvas = new Canvas();
+	var context = canvas.getContext("2d");
 	canvas.width = img.width;
 	canvas.height = img.height;
 	context.mozImageSmoothingEnabled = false;
@@ -36,9 +38,10 @@ function start() {
 			imageData.data[index + 2] = y;
 		}
 	}
+	digit_corners(grayscale_data);
 	//context.putImageData(imageData, 0, 0, 0, 0, imageData.width, imageData.height);
 	//$('body').append(canvas);
-
+	/*
 	result = best_vertical_shear(grayscale_data);
 	//console.log(JSON.stringify(result));
 	for (var i = 0; i < result.length; ++i) {
@@ -54,7 +57,7 @@ function start() {
 	context2.mozImageSmoothingEnabled = false;
 	context2.webkitImageSmoothingEnabled = false;
 	context2.putImageData(imageData, 0, 0, 0, 0, imageData.width, imageData.height);
-	$('body').append(canvas2);
+	$('body').append(canvas2);*/
 }
 
 function variance(list) {
@@ -146,4 +149,149 @@ function best_vertical_shear(grayscale) {
 	console.log("heighest variance: " + heighest_variance);
 	console.log("heighest variance shear: " + heighest_variance_shear);
 	return heighest_variance_image;
+}
+function array_max(arr, start_index, end_index, key){
+	var max_index = start_index;
+	for(var i = start_index+1; i < end_index; ++i){
+		if(arr[max_index][key] < arr[i][key]){
+			max_index = i;
+		}
+	}
+	return arr[max_index];
+}
+function digit_corners(grayscale_image){
+	var horizontal_lines = horizontal_digit_lines(grayscale_image);
+	var vertical_lines = vertical_digit_lines(grayscale_image);
+	
+	//delete middle horizontal line
+	//find intersections of other two lines
+	//return those coordinates
+}
+function horizontal_digit_lines(grayscale_image){
+	var all_variances = horizontal_variances(grayscale_image);
+	var variance_peaks = get_peaks(all_variances);
+	var horizontal_lines = get_horizontal_lines(grayscale_image, variance_peaks);
+	return horizontal_lines;
+}
+function vertical_digit_lines(grayscale_image){
+	var all_variances = vertical_variances(grayscale_image);
+	//console.log(JSON.stringify(all_variances));
+	var variance_peaks = get_peaks(all_variances);
+	//console.log(JSON.stringify(variance_peaks));
+	var vertical_lines = get_vertical_lines(grayscale_image, variance_peaks);
+	return vertical_lines;
+}
+function get_highest_vertical_luminance_lines(grayscale_image, peaks){
+	var all_luminances = new Array();
+	for(var i = 0; i < peaks.length; ++i){
+		var shear = peaks[i].shear;
+		var luminances = get_vertical_average_luminances(grayscale_image, shear);
+		Array.prototype.push.apply(all_luminances, luminances);
+	}
+	all_luminances.sort(function(a, b){
+		return b.average - a.average;
+	});
+	return all_luminances;
+}
+function get_vertical_lines(grayscale_image, peaks){
+	var all_luminances = get_highest_vertical_luminance_lines(grayscale_image, peaks);
+	var result = [all_luminances[0], all_luminances[1]];
+	console.log(JSON.stringify(result));
+	return result;
+}
+function get_horizontal_lines(grayscale_image, peaks){
+	var transposed_image = transpose_image(grayscale_image);
+	var all_luminances = get_highest_vertical_luminance_lines(transposed_image, peaks);
+	var result = [all_luminances[0], all_luminances[1], all_luminances[2]];
+	console.log(JSON.stringify(result));
+	return result;
+}
+function array_key_value(array, key){
+	var res = [];
+	for(var i = 0; i < array.length; ++i){
+		res.push(array[i][key]);
+	}
+	return res;
+}
+function get_peaks(arr){
+	var average_variance = average(array_key_value(arr, "variance"));
+	var in_high_area = false;
+	var high_area_start = 0;
+	var high_values = new Array();
+	var INTERVAL = 5;
+	//console.log("looking for peaks in: "+JSON.stringify(arr));
+	for(var i = 0; i < arr.length; ++i){
+		if(arr[i].variance > average_variance){
+			if(!in_high_area && i > high_area_start + INTERVAL){
+				in_high_area = true;
+				high_area_start = i;
+			}
+		}else{
+			if(in_high_area){
+				high_values.push(array_max(arr,high_area_start, i, "variance"));
+				in_high_area = false;
+				high_area_start = i;
+			}
+		}
+	}
+	if(in_high_area){
+		high_values.push(array_max(arr, high_area_start, arr.length, "variance"));
+	}
+	return high_values;
+}
+function get_vertical_luminance(grayscale_image, start_col, shear){
+	var rows = grayscale_image.length;
+	var line_results = new Array();
+	for(var row = 1; row < rows; ++row){
+		var col = start_col + shear*row/rows;
+		var float_part = col - Math.floor(col);
+		var value = 0;
+		value += grayscale_image[row][Math.floor(col)]*(1-float_part);
+		value += grayscale_image[row][Math.ceil(col)]*float_part;
+		line_results.push(value);
+	}
+	return line_results;
+}
+function get_vertical_average_luminances(grayscale_image, shear){
+	var averages = new Array();
+	var cols = grayscale_image[0].length;
+	for(var top_col = Math.max(-shear,0); top_col < Math.min(cols, cols-shear); top_col ++){
+		averages.push({"shear":shear, "top_col": top_col, "average":average(get_vertical_luminance(grayscale_image, top_col, shear))});
+	}
+	return averages;
+}
+function get_vertical_average_luminances_variance(grayscale_image, shear){
+	var averages = get_vertical_average_luminances(grayscale_image, shear);
+	var v = variance(array_key_value(averages, "average")) / averages.length;
+	return v;
+}
+function vertical_variances(grayscale_image){
+	var rows = grayscale_image.length;
+	var cols = grayscale_image[0].length;
+	var max_shear = 1.0*rows;
+	var shear_step = 1.0;
+	var all_variances = new Array();
+	for(var shear = -max_shear; shear <= max_shear; shear += shear_step){
+		var v = get_vertical_average_luminances_variance(grayscale_image, shear);
+		if(isNaN(v))
+			continue;
+		all_variances.push({"shear":shear, "variance":v});
+	}
+	return all_variances;
+}
+function transpose_image(image){
+	var translated_image = new Array();
+	for(var i = 0; i < image[0].length; ++i){
+		translated_image.push(new Array(image.length));
+	}
+	for(var i = 0; i < image.length; ++i){
+		for(var j = 0; j < image[i].length; ++j){
+			translated_image[j][i] = image[i][j];
+		}
+	}
+	return translated_image;
+}
+function horizontal_variances(grayscale_image){
+	var translated_image = transpose_image(grayscale_image);
+	return vertical_variances(translated_image);
 }
