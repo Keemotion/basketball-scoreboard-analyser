136a01edd7d929b405634f4e2a01d6e099a7c1c4
Floris Kint <floris.kint@gmail.com>
2014-11-07 01:47:56 +0100

    Improve toolbar

--- js/view/toolbar/toolbar.js
+++ js/view/toolbar/toolbar.js
@@ -10,9 +10,9 @@ define(
 			this.messaging_system = messaging_system;
 			this.state_proxy = state_proxy;
 
-			// Mouse Mode: edit mode
-			// Mouse Mode: view edit mode
-			// Mouse Mode: drag mode
+			// Mouse Mode: edition mode
+			// Mouse Mode: canvas drag mode
+			// Mouse Mode: selection mode
 			this.target_div.addClass('btn-toolbar');
 			this.edit_tool_btn = $('<button>')
 				.attr({
@@ -32,12 +32,12 @@ define(
 				.mouseup(function(){
 					$(this).blur();
 				});
-			this.edit_view_tool_btn = $('<button>')
+			this.canvas_tool_btn = $('<button>')
 				.attr({
 					'title' : 'Move the image on the canvas',
 					'data-toggle' : 'button'
 				})
-				.append($('<i>').addClass('fa fa-hand-o-up'))
+				.append($('<span>').addClass('glyphicon glyphicon-hand-up'))
 				.addClass('btn btn-default btn-view-mode')
 				.click(
 				function(){
@@ -50,10 +50,10 @@ define(
 				.mouseup(function(){
 					$(this).blur();
 				});
-			this.drag_tool_btn = $('<button>')
-				.append($('<i>').addClass('fa fa-arrows'))
+			this.selection_tool_btn = $('<button>')
+				.append($('<span>').addClass('glyphicon glyphicon-plus'))
 				.attr({
-					'title' : 'Move selected objects',
+					'title' : 'Select objects',
 					'data-toggle' : 'button'
 				})
 				.addClass('btn btn-default btn-view-mode')
@@ -71,19 +71,36 @@ define(
 			this.mouse_mode_btns = $('<div>').addClass('btn-group').attr(
 				'data-toggle', 'buttons').append(
 				this.edit_tool_btn)
-				.append(this.edit_view_tool_btn).append(
-				this.drag_tool_btn);
+				.append(this.canvas_tool_btn).append(
+				this.selection_tool_btn);
 			self.messaging_system.addEventListener(
 				self.messaging_system.events.MouseModeChanged,
 				new EventListener(this, this.mouseModeChanged));
 			this.edit_tool_btn.click();
 			this.target_div.append(this.mouse_mode_btns);
 
+
+			// load other image
+			this.img_btn = $('<button>')
+				.attr('title', 'Import a new image').append(
+				$('<span>').addClass('glyphicon glyphicon-upload'))
+				.append($('<span>').text('Image')).addClass('btn btn-default').click(function(){
+					var link = document.createElement('input');
+					link.type = "file";
+					$(link).change(function(){
+						self.imageChanged(link);
+					});
+					link.click();
+				})
+				.mouseup(function(){
+					$(this).blur();
+				});
+
 			this.import_export_btns = $('<div>').addClass('btn-group');
 			// load JSON
 			this.load_json_btn = $('<button>').attr('title',
 				'Import JSON file').append(
-				$('<i>').addClass('fa fa-upload')).append(
+				$('<span>').addClass('glyphicon glyphicon-upload')).append(
 				$('<span>').text('JSON')).addClass('btn btn-default')
 				.click(function(){
 					var input = document.createElement('input');
@@ -99,7 +116,7 @@ define(
 			// load PRM
 			this.load_prm_btn = $('<button>').attr('title',
 				'Import PRM file').append(
-				$('<i>').addClass('fa fa-upload')).append(
+				$('<i>').addClass('glyphicon glyphicon-upload')).append(
 				$('<span>').text('PRM')).addClass('btn btn-default')
 				.click(function(){
 					var input = document.createElement('input');
@@ -114,7 +131,7 @@ define(
 				});
 			// export JSON
 			this.download_json_btn = $('<button>').append(
-				$('<i>').addClass('fa fa-download')).append(
+				$('<i>').addClass('glyphicon glyphicon-download')).append(
 				$('<span>').text('JSON'))
 				.attr('title', 'Download JSON').addClass(
 				'btn btn-default').click(
@@ -133,7 +150,7 @@ define(
 			// export PRM
 			this.download_prm_btn = $('<button>')
 				.attr('title', 'Download PRM')
-				.append($('<i>').addClass('fa fa-download'))
+				.append($('<span>').addClass('glyphicon glyphicon-download'))
 				.append($('<span>').text('PRM'))
 				.addClass('btn btn-default')
 				.click(
@@ -149,28 +166,14 @@ define(
 				.mouseup(function(){
 					$(this).blur();
 				});
-			// load other image
-			this.img_btn = $('<button>')
-				.attr('title', 'Import a new image').append(
-				$('<i>').addClass('fa fa-image')).addClass(
-				'btn btn-default').click(function(){
-					var link = document.createElement('input');
-					link.type = "file";
-					$(link).change(function(){
-						self.imageChanged(link);
-					});
-					link.click();
-				})
-				.mouseup(function(){
-					$(this).blur();
-				});
 			this.import_export_btns.append(this.load_json_btn).append(
 				this.load_prm_btn).append(this.download_json_btn)
 				.append(this.download_prm_btn);
-			this.target_div.append(this.import_export_btns).append(
-				$('<div>').addClass('btn-group').append(this.img_btn));
+			this.target_div
+				.append($('<div>').addClass('btn-group').append(this.img_btn))
+				.append(this.import_export_btns);
 
-			// reset canvas view
+			/*// reset canvas view
 			// clear configuration
 			// reset configuration
 			this.reset_btns = $('<div>').addClass('btn-group');
@@ -215,7 +218,7 @@ define(
 			this.reset_btns.append(this.reset_view_btn).append(
 				this.reset_state_btn).append(this.clear_state_btn);
 			this.target_div.append(this.reset_btns);
-
+*/
 			this.other_buttons = $('<div>').addClass('btn-group');
 			// Autofocus
 			this.autofocus_button = $('<button>').addClass(
@@ -227,35 +230,48 @@ define(
 				});
 			this.other_buttons.append(this.autofocus_button);
 			this.target_div.append(this.other_buttons);
+
+			this.other_fields = $('<div>');
+			this.image_name_field = $('<span>').text('');
+			this.other_fields.append(this.image_name_field);
+			this.other_fields.append($('<span>').html('&nbsp;'));
+			this.prm_file_field = $('<span>').text('');
+			this.other_fields.append(this.prm_file_field);
+			this.target_div.append(this.other_fields);
 		};
 
 		ToolBar.prototype.mouseModeChanged = function(signal, data){
 			this.edit_tool_btn.removeClass('active');
-			this.edit_view_tool_btn.removeClass('active');
-			this.drag_tool_btn.removeClass('active');
+			this.canvas_tool_btn.removeClass('active');
+			this.selection_tool_btn.removeClass('active');
 			switch(data.getMode()){
 				case CanvasMouseHandler.MouseModes.EditMode:
 					this.edit_tool_btn.addClass('active');
 					break;
 				case CanvasMouseHandler.MouseModes.CanvasMode:
-					this.edit_view_tool_btn.addClass('active');
+					this.canvas_tool_btn.addClass('active');
 					break;
-				case CanvasMouseHandler.MouseModes.MoveMode:
-					this.drag_tool_btn.addClass('active');
+				case CanvasMouseHandler.MouseModes.SelectionMode:
+					this.selection_tool_btn.addClass('active');
 					break;
 				default:
 					break;
 			}
 		};
+		ToolBar.prototype.extractFileName = function(path){
+			return path.substr(path.lastIndexOf("\\")+1);
+		};
 		ToolBar.prototype.imageChanged = function(component){
 			var self = this;
 			var files = component.files;
+
 			var f = files[0];
 			var reader = new FileReader();
 			reader.onload = function(e){
 				self.messaging_system.fire(
 					self.messaging_system.events.LoadImage,
 					reader.result);
+				self.image_name_field.text("Image: "+self.extractFileName(component.value));
 			};
 			reader.readAsDataURL(f);
 		};
@@ -268,6 +284,7 @@ define(
 				self.messaging_system.fire(
 					self.messaging_system.events.LoadStateFile,
 					new LoadStateEvent(e.target.result));
+				self.prm_file_field.text("File: "+self.extractFileName(component.value));
 			};
 			reader.readAsText(f);
 		};
